8dd8eed05135243040124d9bc6ebf40b7be881ee
Siddharth Garimella <gsidsid@gmail.com>
2018-10-10 16:26:55 -0400

    moved course listings out

--- registrar.py
+++ registrar.py
@@ -1,22 +1,8 @@
+import json
 
 class Registrar(object):
 
-    course_book = {
-    	"DesNat":["MT900"],
-    	"ISIM":["TF900","TF1050","TF1330"],
-        "Discrete": ["MT900", "MT1050"],
-        "Bayesian": ["TF900", "TF1050"],
-        "CompArch": ["TF1050"],
-        "CompBayes": ["TF1330"],
-        "CompRobo": ["TF1330"],
-        "LinII": ["TF1330"],
-        "QEAII": ["MT1330"],
-        "PoE": ["TF900", "TF1050"],
-        "Dynamics": ["TF900"],
-        "SoftDes": ["MT1050"],
-        "UoCD": ["MT900"],
-        "Transport": ["MT1050"]
-    }
+    course_book = json.loads(open('course_book.json').read())
 
     slotMappings = {
         "MT900": "red",
