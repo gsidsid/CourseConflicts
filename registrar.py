b847ffcaeb401ea744009c996ea819c141087652
Siddharth Garimella <gsidsid@gmail.com>
2018-10-10 15:41:52 -0400

    added courses

--- registrar.py
+++ registrar.py
@@ -2,24 +2,31 @@
 class Registrar(object):
 
     course_book = {
-        "Discrete": ["MT900", "MT1040"],
-        "Bayesian": ["TF900", "TF1040"],
-        "CompArch": ["TF1300"],
-        "CompBayes": ["TF1300"],
-        "LinII": ["TF1300"],
-        "QEAII": ["MT1300"],
-        "PoE": ["TF900", "TF1040"]
+    	"DesNat":["MT900"],
+    	"ISIM":["TF900","TF1050","TF1330"],
+        "Discrete": ["MT900", "MT1050"],
+        "Bayesian": ["TF900", "TF1050"],
+        "CompArch": ["TF1050"],
+        "CompBayes": ["TF1330"],
+        "CompRobo": ["TF1330"],
+        "LinII": ["TF1330"],
+        "QEAII": ["MT1330"],
+        "PoE": ["TF900", "TF1050"],
+        "Dynamics": ["TF900"],
+        "SoftDes": ["MT1050"],
+        "UoCD": ["MT900"],
+        "Transport": ["MT1050"]
     }
 
     slotMappings = {
         "MT900": "red",
-        "MT1040": "blue",
-        "MT1300": "green",
-        "MT1510": "yellow",
+        "MT1050": "blue",
+        "MT1330": "green",
+        "MT1520": "yellow",
         "TF900": "purple",
-        "TF1040": "pink",
-        "TF1300": "brown",
-        "TF1510": "orange"
+        "TF1050": "pink",
+        "TF1330": "brown",
+        "TF1520": "orange"
     }
 
     def __init__(self):
