dc2951b0f73f5ca84b65ce9e8976a76f1b56fc27
Kyle Combes <kylecombes@gmail.com>
2018-10-13 17:10:58 -0400

    Connect backtrack coloring with visualizer.

--- Graphing/scheduler.py
+++ Graphing/scheduler.py
@@ -1,23 +1,11 @@
-from graph import Graph
-from registrar import Registrar
-
-import networkx as nx
+from Graphing.graph import Graph
+from Graphing.registrar import Registrar
+from Coloring.color_backtrack import color_graph
 
 R = Registrar()
 G = Graph()
 
-def color_graph_test(g):
-    d = nx.greedy_color(g.G, strategy='independent_set')
-    print(d)
-    for course in R.course_book:
-        g.color_vertex(course,R.slotMappings[str(d[course])])
-
-def color_graph(g):
-    # TODO: Define course book further, implement graph coloring algorithm
-    pass
-
-
 G.build_graph(R.course_book)
-color_graph_test(G)
+color_graph(G)
 G.visualize()
 G.export_graph()
