96072be09c26245df4eb148f231390160fcbea78
Siddharth Garimella <gsidsid@gmail.com>
2018-10-14 14:08:37 -0400

    trying with dyno addition in Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+heroku ps:scale web=1
 web: gunicorn -b :$PORT --pythonpath Visualizations cfvis
--- Visualizations/cfvis.py
+++ Visualizations/cfvis.py
@@ -3,14 +3,14 @@ from networkx.readwrite import json_graph
 import json
 import flask
 import networkx as nx
-import os
+
 
 with open('Visualizations/static/graph.json') as f:
     graph = json.load(f)
 nodes = graph["nodes"]
 links = graph["links"]
 
-port = int(os.environ.get('PORT', 5000))
+
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -30,5 +30,6 @@ def about():
     return flask.render_template("about.html")
 
 if __name__ == '__main__':
-    print('\n When2Discrete deployment complete. \n')
+    import os
+    port = int(os.environ.get('PORT', 33507))
     app.run(host='0.0.0.0',port=port)
